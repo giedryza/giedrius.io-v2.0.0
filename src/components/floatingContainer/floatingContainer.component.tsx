6792103ff24b7c9a6a84b3cd201153a7a60a00c1
Giedrius <giedrius.bla@gmail.com>
2019-11-30 16:07:02 +0200

    add floating container

---
+++ src/components/floatingContainer/floatingContainer.component.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+export enum Position {
+  TOP,
+  BOTTOM,
+}
+
+interface Props {
+  position: Position;
+}
+
+const FloatingContainer: React.FC<Props> = ({ children, position }) => {
+  const getPosition = (pos: Position) => {
+    switch (pos) {
+      case Position.TOP:
+        return { top: 0 };
+      case Position.BOTTOM:
+        return { bottom: 0 };
+      default:
+        return {};
+    }
+  };
+
+  const styles = getPosition(position);
+
+  return (
+    <div className="floating-container container" style={styles}>
+      {children}
+    </div>
+  );
+};
+
+export default FloatingContainer;
