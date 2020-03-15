4d3098f544b1c496864d76c99b86a2cb01636f60
Giedrius <giedrius.bla@gmail.com>
2020-03-16 01:00:01 +0200

    move app outside sections

---
+++ src/app.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import './styles/global/normalize.css';
+import './styles/index.scss';
+import Menu from './components/menu/menu.component';
+import Avatar from './sections/avatar/avatar.component';
+import Experience from './sections/experience/experience.component';
+import Skills from './sections/skills/skills.component';
+import Contacts from './sections/contacts/contacts.component';
+
+const App: React.FC = () => (
+  <>
+    <Menu />
+    <main className="container">
+      <Avatar />
+      <Experience />
+      <Skills />
+      <Contacts />
+    </main>
+  </>
+);
+
+export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './sections/app/app.component';
 import * as serviceWorker from './utils/serviceWorker';
+import App from './app';
 
 const app = <App />;
 const root = document.getElementById('root') as HTMLDivElement;
