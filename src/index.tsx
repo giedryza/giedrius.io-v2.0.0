8bffdb85a0c3fd936873e179833cc13ee57c893c
Giedrius <giedrius.bla@gmail.com>
2020-03-16 13:05:59 +0200

    remove react-axe

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './utils/serviceWorker';
-import { Env } from './types/env';
-import { initReactAxe } from './utils/reactAxe';
 import App from './app';
-
-if (process.env.NODE_ENV === Env.dev) {
-  initReactAxe(React, ReactDOM);
-}
 
 const app = <App />;
 const root = document.getElementById('root') as HTMLDivElement;
