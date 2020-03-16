cd2713dc5108eb1238cb770f05cf6db559830dbf
Giedrius <giedrius.bla@gmail.com>
2020-03-16 02:02:52 +0200

    add react axe util

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './utils/serviceWorker';
 import { Env } from './types/env';
+import { initReactAxe } from './utils/reactAxe';
 import App from './app';
 
 if (process.env.NODE_ENV === Env.dev) {
-  const TIMING_DELAY = 1000;
-  // eslint-disable-next-line global-require
-  const axe = require('react-axe');
-  axe(React, ReactDOM, TIMING_DELAY);
+  initReactAxe(React, ReactDOM);
 }
 
 const app = <App />;
