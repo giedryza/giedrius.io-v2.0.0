dec778349ab323eccdd99fe9c561784556a2c367
Giedrius <giedrius.bla@gmail.com>
2020-03-24 21:07:14 +0200

    extract render

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
 import * as serviceWorker from 'utils/serviceWorker';
 import App from 'app';
 
 const app = <App />;
 const root = document.getElementById('root') as HTMLDivElement;
 
-ReactDOM.render(app, root);
+render(app, root);
 
 serviceWorker.register();
